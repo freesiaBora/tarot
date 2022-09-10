aa9d22aab653c2d0ab35a5b165eb5123a8c9a8ca
seobora <freesiabora@gmail.com>
2022-09-11 03:20:12 +0900

    create new project

---
+++ src/main/java/com/bora/tarot/security/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.bora.tarot.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
